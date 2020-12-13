663a08b176cefd1d1ae6a015291d25c13a7b5df2
Drasko Draskovic <drasko.draskovic@gmail.com>
2020-12-14 00:35:13 +0100

    Simplify main
    Signed-off-by: Drasko Draskovic <drasko.draskovic@gmail.com>

--- main.go
+++ main.go
@@ -25,17 +25,32 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, data)
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tmpl/test.html")
+	if err != nil {
+		panic(err)
+	}
+
+	data := struct {
+		Name string
+	}{"John Smith"}
+
+	t.Execute(w, data)
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	// Serve static files
+	fs := http.FileServer(http.Dir(staticDir))
 	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir(staticDir)).ServeHTTP(w, r)
+		fs.ServeHTTP(w, r)
 	})
 
 	// Routes
 	r.Get("/", rootHandler)
+	r.Get("/test", testHandler)
 
 	http.ListenAndServe(":3000", r)
 }
